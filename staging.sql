311b10d55457e375e50f40a05fae10fc6e10f151
anjali1153 <147938094+anjali1153@users.noreply.github.com>
2026-06-06 11:58:45 +0530

    SQL Datawarehouse

---
+++ staging.sql
@@ -0,0 +1,23 @@
+create table stg_orders(
+row_id INT,
+order_id VARCHAR(50),
+order_date DATE,
+ship_date DATE,
+ship_mode VARCHAR(50),
+customer_id VARCHAR(50),
+customer_name VARCHAR(100),
+segment VARCHAR(50),
+country VARCHAR(100),
+city VARCHAR(100),
+state_name VARCHAR(100),
+postal_code INT,
+region VARCHAR(50),
+product_id VARCHAR(50),
+category VARCHAR(50),
+sub_category VARCHAR(50),
+product_name TEXT,
+sales DECIMAL(10,2),
+quantity INT,
+discount DECIMAL(5,2),
+profit DECIMAL(10,2)
+);
